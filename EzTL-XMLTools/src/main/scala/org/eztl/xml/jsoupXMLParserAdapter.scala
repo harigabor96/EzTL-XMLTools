7c7efbaa736849fc9918f017fb59d10293c34daa
Hári Gábor <gabor.hari@ecobit.hu>
2022-11-25 13:07:57 +0100

    adjusted to return null like default spark formats

--- EzTL-XMLTools/src/main/scala/org/eztl/xml/jsoupXMLParserAdapter.scala
+++ EzTL-XMLTools/src/main/scala/org/eztl/xml/jsoupXMLParserAdapter.scala
@@ -17,6 +17,7 @@ object JsoupXMLParserAdapter extends GenericXMLParserAdapter {
   }
 
   override def _cleanup_xml_tags(XMLString: String): String = {
+    if (XMLString == null) return null
     val outputSettings = new Document.OutputSettings()
     outputSettings.prettyPrint(false)
     Jsoup.clean(XMLString, "", Whitelist.none(), outputSettings)
@@ -24,7 +25,9 @@ object JsoupXMLParserAdapter extends GenericXMLParserAdapter {
   override def cleanup_xml_tags: UserDefinedFunction = udf(_cleanup_xml_tags(_))
 
   override def _getall_xml_elements(elementTag: String)(XMLString: String): Array[String] = {
-    if (XMLString == "") return Array[String]("")
+    if (elementTag == null || elementTag == "") throw new Exception("Invalid global parameters!")
+    if (XMLString == "" || XMLString == null) return Array[String](null)
+
     val elementArray = ArrayBuffer[String]()
 
     parseXML(XMLString)
@@ -33,26 +36,35 @@ object JsoupXMLParserAdapter extends GenericXMLParserAdapter {
         elementArray += element.toString
       )
 
+    if (elementArray.isEmpty) return Array[String](null)
+
     elementArray.toArray
   }
   override def getall_xml_elements(elementTag: String): UserDefinedFunction = udf(_getall_xml_elements(elementTag)(_))
 
   override def _get_xml_element(elementTag: String)(XMLString: String): String = {
+    if (elementTag == null || elementTag == "") throw new Exception("Invalid global parameters!")
+    if (XMLString == "" || XMLString == null) return null
+
     val XMLTags = parseXML(XMLString).getElementsByTag(elementTag)
 
     if (1 < XMLTags.size()) throw new Exception("Not a single element!")
-    if (XMLTags.size() == 0) return ""
+    if (XMLTags.size() == 0) return null
 
     XMLTags.first().toString
   }
   override def get_xml_element(elementTag: String): UserDefinedFunction = udf(_get_xml_element(elementTag)(_))
 
   override def _get_xml_attribute(elementTag: String, attribute: String)(XMLString: String): String = {
+    if (elementTag == null || elementTag == "" || attribute == null || attribute == "") throw new Exception("Invalid global parameters!")
+    if (XMLString == "" || XMLString == null) return null
+
     val XMLTags = parseXML(XMLString).getElementsByTag(elementTag)
 
     if (1 < XMLTags.size()) throw new Exception("Not a single element!")
-    if (XMLTags.size() == 0) return ""
+    if (XMLTags.size() == 0) return null
 
+    if (!XMLTags.first().hasAttr(attribute)) return null
     XMLTags.first().attr(attribute)
   }
   override def get_xml_attribute(elementTag: String, attribute: String): UserDefinedFunction = udf(_get_xml_attribute(elementTag, attribute)(_))
--- EzTL-XMLTools/src/test/scala/suites/org/eztl/xml/jsoupXMLParserAdapter.scala
+++ EzTL-XMLTools/src/test/scala/suites/org/eztl/xml/jsoupXMLParserAdapter.scala
@@ -6,50 +6,114 @@ import org.scalatest.funsuite.AnyFunSuite
 class JsoupXMLParserAdapter extends AnyFunSuite {
 
   test("_cleanup_xml_tags()") {
+    assert(_cleanup_xml_tags(null) == null)
     assert(_cleanup_xml_tags("") == "")
     assert(_cleanup_xml_tags("<tag>a</tag>") == "a")
     assert(_cleanup_xml_tags("<tag>a</tag><element>b</element>") == "ab")
-    assertThrows[Exception](_cleanup_xml_tags(null))
   }
 
   test("_getall_xml_elements()"){
+    //Empty or null input string
     assert(
-      _getall_xml_elements("tag")("<tag>a</tag><tag>b</tag>")(0) == "<tag>a</tag>" &&
-      _getall_xml_elements("tag")("<tag>a</tag><tag>b</tag>")(1) == "<tag>b</tag>"
+      _getall_xml_elements("tag")("")
+        .sameElements(Array[String](null))
     )
     assert(
-      _getall_xml_elements("tag")("<tag>a</tag><element>x</element><tag>b</tag>")(0) == "<tag>a</tag>" &&
-      _getall_xml_elements("tag")("<tag>a</tag><element>x</element><tag>b</tag>")(1) == "<tag>b</tag>"
+      _getall_xml_elements("tag")(null)
+        .sameElements(Array[String](null))
     )
-    assert(_getall_xml_elements("tag")("")(0) == "")
+    //Tag not found
+    assert(
+      _getall_xml_elements("element")("<tag>a</tag><tag>b</tag>")
+        .sameElements(Array[String](null))
+    )
+    //Tag found
+    assert(
+      _getall_xml_elements("tag")("<tag>a</tag><tag>b</tag>")
+        .sameElements(Array[String]("<tag>a</tag>","<tag>b</tag>"))
+    )
+    assert(
+      _getall_xml_elements("tag")("<tag>a</tag><element>x</element><tag>b</tag>")
+        .sameElements(Array[String]("<tag>a</tag>","<tag>b</tag>"))
+    )
+    assert(
+      _getall_xml_elements("tag")("<tag><v>a</v></tag><tag>b</tag>")
+        .sameElements(Array[String]("<tag><v>a</v></tag>", "<tag>b</tag>"))
+    )
+    //Invalid global parameters
     assertThrows[Exception](_getall_xml_elements("")("<tag>a</tag><tag>b</tag>"))
     assertThrows[Exception](_getall_xml_elements(null)("<tag>a</tag><tag>b</tag>"))
   }
 
   test("_get_xml_element()") {
-    assert(_get_xml_element("tag")("<tag>a</tag>") == "<tag>a</tag>")
-    assert(_get_xml_element("tag")("<tag><v>a</v></tag><element>e</element>") == "<tag><v>a</v></tag>")
-    assert(_get_xml_element("tag")("<tag>a</tag><element>e</element>") == "<tag>a</tag>")
-    assert(_get_xml_element("tag")("") == "")
-    assert(_get_xml_element("tag")("<element></element>") == "")
-    assertThrows[Exception](_get_xml_element("tag")(null))
-    assertThrows[Exception](_get_xml_element(null)("<tag>a</tag>"))
+    //Empty or null input string
+    assert(
+      _get_xml_element("tag")("") == null
+    )
+    assert(
+      _get_xml_element("tag")(null) == null
+    )
+    //Tag not found
+    assert(
+      _get_xml_element("tag")("<element></element>") == null
+    )
+    //Tag found
+    assert(
+      _get_xml_element("tag")("<tag></tag>") == "<tag></tag>"
+    )
+    assert(
+      _get_xml_element("tag")("<tag>a</tag>") == "<tag>a</tag>"
+    )
+    assert(
+      _get_xml_element("tag")("<tag><v>a</v></tag><element>e</element>") == "<tag><v>a</v></tag>"
+    )
+    assert(
+      _get_xml_element("tag")("<tag>a</tag><element>e</element>") == "<tag>a</tag>"
+    )
+    //Multiple tags of the same kind
     assertThrows[Exception](_get_xml_element("tag")("<tag>a</tag><tag>e</tag>"))
+    //Invalid global parameters
+    assertThrows[Exception](_get_xml_element(null)("<tag>a</tag>"))
     assertThrows[Exception](_get_xml_element("")("<tag>a</tag>"))
   }
 
   test("_get_xml_attribute()") {
-    assert(_get_xml_attribute("tag","attr")("") == "")
-    assert(_get_xml_attribute("tag","id")("""<tag id="1"></tag>""") == "1")
-    assert(_get_xml_attribute("tag","id")("""<tag></tag>""") == "")
-    assert(_get_xml_attribute("tag","id")("""<tag id=""></tag>""") == "")
+    //Empty or null input string
+    assert(
+      _get_xml_attribute("tag","attr")("") == null
+    )
+    assert(
+      _get_xml_attribute("tag", "attr")(null) == null
+    )
+    //Tag not found
+    assert(
+      _get_xml_attribute("tag", "id")("""<element></element>""") == null
+    )
+    //Attribute not found
+    assert(
+      _get_xml_attribute("tag", "id")("""<tag></tag>""") == null
+    )
+    //Tag and attribute found
+    assert(
+      _get_xml_attribute("tag","id")("""<tag id="1"></tag>""") == "1"
+    )
+    assert(
+      _get_xml_attribute("tag","id")("""<tag id=""></tag>""") == ""
+    )
+    assert(
+      _get_xml_attribute("tag", "id")("""<tag id="1"></tag><element></element>""") == "1"
+    )
+    assert(
+      _get_xml_attribute("tag", "id")("""<tag id="1"><element>a</element></tag>""") == "1"
+    )
+    //Multiple tags of the same kind
     assertThrows[Exception](_get_xml_attribute("tag","id")("""<tag id="1"></tag><tag id="2"></tag>"""))
     assertThrows[Exception](_get_xml_attribute("tag","id")("""<tag></tag><tag></tag>"""))
+    //Invalid global parameters
     assertThrows[Exception](_get_xml_attribute("","id")("""<tag id="1"></tag>"""))
     assertThrows[Exception](_get_xml_attribute(null,"id")("""<tag id="1"></tag>"""))
     assertThrows[Exception](_get_xml_attribute("tag", "")("""<tag id="1"></tag>"""))
     assertThrows[Exception](_get_xml_attribute("tag", null)("""<tag id="1"></tag>"""))
-    assertThrows[Exception](_get_xml_attribute("tag","id")(null))
   }
 
 }
